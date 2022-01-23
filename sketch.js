c0824a76203461dc068bd30f392834ac350d6d11
Siha0 <96207639+Siha0@users.noreply.github.com>
2022-01-22 19:37:29 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function preload(){
 function setup(){
   
 //create the canvas and adjust the window sizes to suit the device 
-
+createcanvas(windowWidth,windowHeight)
 path=createSprite(width/2,200);
 path.addImage(pathImg);
 path.velocityY = 4;
@@ -50,7 +50,9 @@ function draw() {
   boy.collide(edges);
   
   //code to reset the background
-
+if(path.x<0){
+path.x=path.width/2
+}
     createCash();
     createDiamonds();
     createJwellery();
@@ -101,7 +103,7 @@ function draw() {
 function createCash() {
   if (World.frameCount % 200 == 0) {
    // Modify the positions of cash 
-    var cash = createSprite(Math.round(random(50, 350),40, 10, 10));
+    var cash = createSprite(Math.round(random(50, width-50),40, 10, 10));
     cash.addImage(cashImg);
   cash.scale=0.12;
   cash.velocityY = 5;
@@ -114,7 +116,7 @@ function createDiamonds() {
   if (World.frameCount % 320 == 0) {
        // Modify the positions of diamonds 
 
-    var diamonds = createSprite(Math.round(random(50, 350),40, 10, 10));
+    var diamonds = createSprite(Math.round(random(50, width-50),40, 10, 10));
     diamonds.addImage(diamondsImg);
   diamonds.scale=0.03;
   diamonds.velocityY = 5;
@@ -127,7 +129,7 @@ function createJwellery() {
   if (World.frameCount % 410 == 0) {
     //   Modify the positions of jwellery to make them spawn throughout the available screen size.
 
-    var jwellery = createSprite(Math.round(random(50, 350),40, 10, 10));
+    var jwellery = createSprite(Math.round(random(50, width-50),40, 10, 10));
     jwellery.addImage(jwelleryImg);
   jwellery.scale=0.13;
   jwellery.velocityY = 5;
@@ -140,7 +142,7 @@ function createSword(){
   if (World.frameCount % 530 == 0) {
     //   Modify the positions of sword to make them spawn throughout the available screen size.
 
-    var sword = createSprite(Math.round(random(50, 350),40, 10, 10));
+    var sword = createSprite(Math.round(random(50, width-50),40, 10, 10));
     sword.addImage(swordImg);
   sword.scale=0.1;
   sword.velocityY = 4;
